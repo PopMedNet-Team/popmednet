8a11f0f3f2c59f8356aebf38340ea12ee3af01a7
Jeffrey Ladd <jladd@jeffreyladd.net>
2021-03-30 00:56:07 +0000

    PMN-2021.1 Release

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Harvard Pilgrim Health Care Institute")]
 [assembly: AssemblyProduct("Distributed Network Solution")]
-[assembly: AssemblyCopyright("Copyright © PopMedNet 2011-2019")]
+[assembly: AssemblyCopyright("Copyright © PopMedNet 2011-2021")]
 [assembly: ComVisible(false)]
 //Please Also update The Click Once version under Lpp.Dns.DataMartClient Properties > publish > Version Number 
-[assembly: AssemblyVersion( /*<VERSION>*/ "6.21.0.0" /*</VERSION>*/ )]
-[assembly: AssemblyFileVersion( /*<VERSION>*/ "6.21.0.0" /*</VERSION>*/ )]
-[assembly: AssemblyInformationalVersion("2021.1")]
+[assembly: AssemblyVersion( /*<VERSION>*/ "7.0.0.0" /*</VERSION>*/ )]
+[assembly: AssemblyFileVersion( /*<VERSION>*/ "7.0.0.0" /*</VERSION>*/ )]
+[assembly: AssemblyInformationalVersion("2021.2")]
